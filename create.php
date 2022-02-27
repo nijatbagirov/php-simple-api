08fa548b45bf072d75c4c04189fee7c3c06b5308
nijatbagirov <nicatbagirov1@gmail.com>
2022-02-28 00:28:47 +0400

    Update create.php

--- create.php
+++ create.php
@@ -4,8 +4,8 @@
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    include_once 'config/db.php';
-    include_once 'class/users.php';
+    include_once '../config/db.php';
+    include_once '../class/users.php';
     $database = new Database();
     $db = $database->getConnection();
     $item = new User($db);
