b269420d78a4ae28564363014e292f609e8fe167
nijatbagirov <nicatbagirov1@gmail.com>
2022-02-28 00:29:58 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     
-    include_once '../config/db.php';
-    include_once '../class/users.php';
+    include_once 'config/db.php';
+    include_once 'class/users.php';
     $database = new Database();
     $db = $database->getConnection();
     $items = new User($db);
